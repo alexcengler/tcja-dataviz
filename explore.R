f6db9455fe3e9ad77e23d95c7222a22bc7e7f36b
alexcengler <alexcengler@gmail.com>
2018-08-08 12:40:45 -0500

    Data description notes

--- explore.R
+++ explore.R
@@ -8,5 +8,33 @@ library(tidyverse)
 ## Read in data:
 tcja <- read_csv("tcja_tiny.csv")
 
-dim(tcja)
+## 6910 Models Runs:
+dim(tcja) 
 glimpse(tcja)
+
+
+View(tcja[1,])
+
+
+
+## Outcome Variables for After Tax Income: 
+# By INCOME LEVELS: PctChginAftTaxIncPercent_LEVEL_ 2-9
+# BY INCOME PERCENTILES: PctChginAftTaxIncPercent_PCT_ 1-18
+# PctofPreTaxIncClPercent_LEVEL_1 - what is this?
+
+## Outcome Variables for Revenue Change:
+# REV_CHG
+
+
+
+
+
+
+
+
+
+## Outcome variables thoughts:
+## How do people under 30k do? How do they do relative to people over 100K?
+
+
+
